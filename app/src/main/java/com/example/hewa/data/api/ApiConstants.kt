af5a5464e2034cc0c79307ce83313d4d74a018ca
Hoodlab <89312679+Hoodlab@users.noreply.github.com>
2023-02-05 23:11:57 +0300

    Update ApiConstants.kt

--- app/src/main/java/com/example/hewa/data/api/ApiConstants.kt
+++ app/src/main/java/com/example/hewa/data/api/ApiConstants.kt
@@ -3,7 +3,7 @@ package com.example.hewa.data.api
 object ApiConstants {
     const val API_BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val END_POINT = "weather"
-    const val API_KEY = "5efafcadd36adfe0cd258faa95ba69f7"
+    const val API_KEY = "API_KEY"
     fun iconURl(code:String) = "https://openweathermap.org/img/wn/$code@2x.png"
 }
 
